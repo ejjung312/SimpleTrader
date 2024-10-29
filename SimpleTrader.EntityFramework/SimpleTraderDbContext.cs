a6ad82e0d2e9b9479d977eeb55d8ffeed7e330b8
한국벤처경영원 <ejjung312@naver.com>
2024-10-29 13:23:11 +0900

    초기 커밋.

---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=127.0.0.1;Database=dbcsharp;User=csharp;Password=csharp!@;";
+
+            optionsBuilder.UseMySql(connectionString, MySqlServerVersion.AutoDetect(connectionString));
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
