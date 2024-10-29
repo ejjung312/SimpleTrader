52143609e796f8fcec20394e3777df9ae9629f1a
kofund <ejjung312@naver.com>
2024-10-29 16:47:22 +0900

    WPF in .NET Core and MVVM Navigation

---
+++ SimpleTrader.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SimpleTrader.WPF.ViewModels;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SimpleTrader.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.DataContext = new MainViewModel();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
