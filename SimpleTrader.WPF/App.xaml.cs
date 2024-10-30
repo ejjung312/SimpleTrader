445cbe1ab9b530cd630503a0c228dbe6a2d4771d
kofund <ejjung312@naver.com>
2024-10-30 15:36:24 +0900

    DataService 공통 소스 분리

--- SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader.WPF/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using SimpleTrader.FinancialModelingPrepAPI.Services;
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.Domain.Services.TransactionServices;
+using SimpleTrader.EntityFramework.Services;
+using SimpleTrader.FinancialModelingPrepAPI.Services;
 using SimpleTrader.WPF.ViewModels;
 using System.Windows;
 
@@ -9,13 +13,19 @@ namespace SimpleTrader.WPF
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
+            IDataService<Account> accountService = new AccountDataService(new EntityFramework.SimpleTraderDbContextFactory());
+            IStockPriceService stockPriceService = new StockPriceService();
+            IBuyStockService buyStockService = new BuyStockService(stockPriceService, accountService);
+
+            Account buyer = await accountService.Get(2);
+
+            await buyStockService.BuyStock(buyer, "T", 1);
+
             Window window = new MainWindow();
             window.DataContext = new MainViewModel();
             window.Show();
-
-            new StockPriceService().GetPrice("aaa");
 
             base.OnStartup(e);
         }
