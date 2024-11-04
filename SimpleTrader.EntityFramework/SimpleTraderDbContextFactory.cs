04abbdf0f779d5dac6155916c9deb9815fa5972a
정은지 <ejjung312@naver.com>
2024-11-04 15:47:21 +0900

    appsettings.json

--- SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
+++ SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 
 namespace SimpleTrader.EntityFramework
 {
-    public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext>
+    public class SimpleTraderDbContextFactory
     {
-        public SimpleTraderDbContext CreateDbContext(string[] args = null)
+        private readonly string _connectionString;
+
+        public SimpleTraderDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SimpleTraderDbContext CreateDbContext()
         {
             var options = new DbContextOptionsBuilder<SimpleTraderDbContext>();
-            string connectionString = "Server=127.0.0.1;Database=dbcsharp;User=csharp;Password=csharp!@;";
             
-            options.UseMySql(connectionString, MySqlServerVersion.AutoDetect(connectionString));
+            options.UseMySql(_connectionString, MySqlServerVersion.AutoDetect(_connectionString),
+                b => b.MigrationsAssembly("SimpleTrader.EntityFramework"));
 
             return new SimpleTraderDbContext(options.Options);
         }
