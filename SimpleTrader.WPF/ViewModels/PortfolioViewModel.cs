e0fb497777ac7e573505a2c29d1005f5d8c1524e
정은지 <ejjung312@naver.com>
2024-11-05 10:42:07 +0900

    Reusable Controls

--- SimpleTrader.WPF/ViewModels/PortfolioViewModel.cs
+++ SimpleTrader.WPF/ViewModels/PortfolioViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace SimpleTrader.WPF.ViewModels
+﻿using SimpleTrader.WPF.State.Assets;
+
+namespace SimpleTrader.WPF.ViewModels
 {
     public class PortfolioViewModel : ViewModelBase
     {
+        public AssetListingViewModel AssetListingViewModel { get; }
+
+        public PortfolioViewModel(AssetStore assetStore)
+        {
+            AssetListingViewModel = new AssetListingViewModel(assetStore);
+        }
     }
 }
